a42efc728cf0b6f79b2155407932fec86e8ba6eb
Daniel P. Berrange <berrange@redhat.com>
2012-01-30 16:54:00 +0000

    Add support for --system arg to autogen.sh

--- autogen.sh
+++ autogen.sh
@@ -30,10 +30,23 @@ if test "$DIE" -eq 1; then
 	exit 1
 fi
 
-if test -z "$*"; then
-	echo "I am going to run ./configure with not arguments - if you "
-        echo "wish to pass any extra arguments to it, please specify them on "
-        echo "the $0 command line."
+EXTRA_ARGS=""
+if test "x$1" = "x--system"; then
+    shift
+    prefix=/usr
+    libdir=$prefix/lib
+    sysconfdir=/etc
+    localstatedir=/var
+    if [ -d /usr/lib64 ]; then
+      libdir=$prefix/lib64
+    fi
+    EXTRA_ARGS="--prefix=$prefix --sysconfdir=$sysconfdir --localstatedir=$localstatedir --libdir=$libdir"
+    echo "Running ./configure with $EXTRA_ARGS $@"
+else
+    if test -z "$*" ; then
+        echo "I am going to run ./configure with no arguments - if you wish "
+        echo "to pass any to it, please specify them on the $0 command line."
+    fi
 fi
 
 libtoolize --copy --force
@@ -45,7 +58,7 @@ autoconf
 
 cd $THEDIR
 
-$srcdir/configure "$@" && {
+$srcdir/configure $EXTRA_ARGS "$@" && {
     echo 
     echo "Now type 'make' to compile virt-viewer."
 }

d9825f3c747000fe39ed79aa8e41566bdf30fcd9
Daniel P. Berrange <berrange@redhat.com>
2012-02-01 14:46:53 +0000

    Import GNULIB rules for syntax checking code

--- autobuild.sh
+++ autobuild.sh
@@ -3,6 +3,8 @@
 set -e
 set -v
 
+test -n "$1" && RESULTS=$1 || RESULTS=results.log
+
 # Make things clean.
 test -f Makefile && make -k distclean || :
 
@@ -29,6 +31,16 @@ if test $? = 0 ; then
   make
   make install
 fi
+
+# set -o pipefail is a bashism; this use of exec is the POSIX alternative
+exec 3>&1
+st=$(
+  exec 4>&1 >&3
+  { make syntax-check 2>&1 3>&- 4>&-; echo $? >&4; } | tee "$RESULTS"
+)
+exec 3>&-
+test "$st" = 0
+
 
 rm -f *.tar.gz
 make dist

63feed3d4e5f994d4b7e01dcc3863d2da41e1aa3
Yonit Halperin <yhalperi@redhat.com>
2012-04-03 18:48:09 +0100

    Add support for the SPICE properties disable-effects & color-depth
    Signed-off-by: Yonit Halperin <yhalperi@redhat.com>
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

--- src/remote-viewer.c
+++ src/remote-viewer.c
@@ -583,7 +583,9 @@ spice_ctrl_notified(SpiceCtrlController *ctrl,
         g_str_equal(pspec->name, "port") ||
         g_str_equal(pspec->name, "password") ||
         g_str_equal(pspec->name, "ca-file") ||
-        g_str_equal(pspec->name, "enable-smartcard")) {
+        g_str_equal(pspec->name, "enable-smartcard") ||
+        g_str_equal(pspec->name, "color-depth") ||
+        g_str_equal(pspec->name, "disable-effects")) {
         g_object_set_property(G_OBJECT(session), pspec->name, &value);
     } else if (g_str_equal(pspec->name, "sport")) {
         g_object_set_property(G_OBJECT(session), "tls-port", &value);

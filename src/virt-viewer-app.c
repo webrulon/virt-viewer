06d62c4b02015601c17c90c310f63ee1a92410f0
Daniel P. Berrange <berrange@redhat.com>
2012-04-25 15:31:05 +0100

    Really fix debug output on glib >= 2.31

--- src/virt-viewer-app.c
+++ src/virt-viewer-app.c
@@ -172,6 +172,18 @@ static guint signals[SIGNAL_LAST];
 void
 virt_viewer_app_set_debug(gboolean debug)
 {
+#if GLIB_CHECK_VERSION(2, 31, 0)
+    if (debug) {
+        gchar *doms = getenv("G_MESSAGES_DEBUG");
+        if (!doms) {
+            setenv("G_MESSAGES_DEBUG", G_LOG_DOMAIN, 1);
+        } else if (!strstr(doms, G_LOG_DOMAIN)) {
+            gchar *newdoms = g_strdup_printf("%s %s", doms, G_LOG_DOMAIN);
+            setenv("G_MESSAGES_DEBUG", newdoms, 1);
+            g_free(newdoms);
+        }
+    }
+#endif
     doDebug = debug;
 }
 

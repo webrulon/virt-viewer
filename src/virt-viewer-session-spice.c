d0d226ca8fddc41ca7b91ea3e18e551a06f1fc00
Daniel P. Berrange <berrange@redhat.com>
2012-05-02 13:00:38 +0100

    Change 'OK' button to 'Close' button in USB device selection
    The USB device selection applies immediately, so the dialog
    should be using 'Close' instead of 'OK' for its primary button

--- src/virt-viewer-session-spice.c
+++ src/virt-viewer-session-spice.c
@@ -379,7 +379,7 @@ virt_viewer_session_spice_usb_device_selection(VirtViewerSession *session,
     /* Create the widgets */
     dialog = gtk_dialog_new_with_buttons(_("Select USB devices for redirection"), parent,
                                          GTK_DIALOG_MODAL | GTK_DIALOG_DESTROY_WITH_PARENT,
-                                         GTK_STOCK_OK, GTK_RESPONSE_ACCEPT,
+                                         GTK_STOCK_CLOSE, GTK_RESPONSE_ACCEPT,
                                          NULL);
     gtk_dialog_set_default_response(GTK_DIALOG(dialog), GTK_RESPONSE_ACCEPT);
     gtk_container_set_border_width(GTK_CONTAINER(dialog), 12);

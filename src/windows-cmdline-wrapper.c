dca7397f16f1b7e297be6a0c6b56f5d70ffc02e7
Marc-André Lureau <marcandre.lureau@gmail.com>
2012-05-16 17:33:43 +0200

    win32: there is no CONERR$, only CONOUT$

--- src/windows-cmdline-wrapper.c
+++ src/windows-cmdline-wrapper.c
@@ -30,7 +30,7 @@
  *   if (AttachConsole(ATTACH_PARENT_PROCESS) != 0) {
  *       freopen("CONIN$", "r", stdin);
  *       freopen("CONOUT$", "w", stdout);
- *       freopen("CONERR$", "w", stderr);
+ *       freopen("CONOUT$", "w", stderr);
  *       dup2(fileno(stdin), STDIN_FILENO);
  *       dup2(fileno(stdout), STDOUT_FILENO);
  *       dup2(fileno(stderr), STDERR_FILENO);
